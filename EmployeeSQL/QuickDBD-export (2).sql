5879562ccbd6bcbffc1a525de225d1cff30c3fe8
niganmarin <niganmarin@gmail.comgit config --global user.name niganmaringit config --global user.email niganmarin@gmail.com>
2023-06-05 21:56:55 -0500

    added schema

---
+++ EmployeeSQL/QuickDBD-export (2).sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/Hk5ETY
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR   NOT NULL,
+    "description" VARCHAR   NOT NULL,
+    "goal" INT   NOT NULL,
+    "pledged" INT   NOT NULL,
+    "outcome" VARCHAR   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR   NOT NULL,
+    "currency" VARCHAR   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_ids" VARCHAR   NOT NULL,
+    "subcategory_ids" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "email" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_ids" VARCHAR   NOT NULL,
+    "category" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_ids"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_ids" VARCHAR   NOT NULL,
+    "subcategory" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_ids"
+     )
+);
+
+ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "campaign" ("contact_id");
+
+ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_ids" FOREIGN KEY("category_ids")
+REFERENCES "campaign" ("category_ids");
+
+ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_ids" FOREIGN KEY("subcategory_ids")
+REFERENCES "campaign" ("subcategory_ids");
+
